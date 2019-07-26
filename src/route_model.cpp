aa6aee3854895ea1fffad4c544413bd121db8838
Bander Alshammari <bannadr1@hotmail.com>
2019-07-26 18:03:25 +0300

    fix bug add if not exist

--- src/route_model.cpp
+++ src/route_model.cpp
@@ -13,7 +13,7 @@ void RouteModel::CreateNodeToRoadHashmap(){
     for(auto road : Roads()){
         if(road.type != Model::Road::Type::Footway){
             for(auto node_idx : Ways()[road.way].nodes){
-                if(node_to_road.find(node_idx) != node_to_road.end()){
+                if(node_to_road.find(node_idx) == node_to_road.end()){
                     node_to_road[node_idx] = vector<const Model::Road*> ();
                 }
                 node_to_road[node_idx].push_back(&road);
