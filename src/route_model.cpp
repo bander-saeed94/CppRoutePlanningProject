c9fb4500b8abd88813cabe161dca89d30f93d9d3
Bander Alshammari <bannadr1@hotmail.com>
2019-07-26 19:30:41 +0300

    derefernce closet_node pointer before passing  to distance func

--- src/route_model.cpp
+++ src/route_model.cpp
@@ -28,7 +28,7 @@ RouteModel::Node* RouteModel::Node::FindNeighbor(vector<int> node_indices){
     for(int index : node_indices){
         node = parent_model->SNodes()[index];
         if(!current_node.visited && this->distance(node) != 0){
-            if(closest_node != nullptr || this->distance(node) < this->distance(closest_node)){
+            if(closest_node != nullptr || this->distance(node) < this->distance(*closest_node)){
                 closest_node = &parent_model->SNodes()[index];
             }
         }
