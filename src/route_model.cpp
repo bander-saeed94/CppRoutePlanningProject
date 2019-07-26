8613c11358177b5d3f497a6ad2776bc69aaa19d8
Bander Alshammari <bannadr1@hotmail.com>
2019-07-26 15:25:06 +0300

    initilize nodes in route model

--- src/route_model.cpp
+++ src/route_model.cpp
@@ -2,5 +2,8 @@
 #include <iostream>
 
 RouteModel::RouteModel(const std::vector<std::byte> &xml) : Model(xml) {
-
+    int counter = 0;
+    for(auto node: this->Nodes()){
+        m_Nodes.push_back(Node(counter++, this, node));
+    }
 }
