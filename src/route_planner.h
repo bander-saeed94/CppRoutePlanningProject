f455c7e3c9535c1a3beabd9c5c1888bc8c2ee851
Bander Alshammari <bannadr1@hotmail.com>
2019-07-27 14:36:46 +0300

    start and end pointter for route planner

--- src/route_planner.h
+++ src/route_planner.h
@@ -6,15 +6,22 @@
 #include <string>
 #include "route_model.h"
 
+class RoutePlanner
+{
+public:
+  RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y);
+  // Add public variables or methods declarations here.
+  float GetDistance() const
+  {
+    return distance;
+  }
 
-class RoutePlanner {
-  public:
-    RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y);
-    // Add public variables or methods declarations here.
-
-  private:
-    // Add private variables or methods declarations here.
-    RouteModel &m_Model;
+private:
+  // Add private variables or methods declarations here.
+  RouteModel &m_Model;
+  RouteModel::Node *start_node = nullptr;
+  RouteModel::Node *end_node = nullptr;
+  float distance = 0.0; //will hold the total distance for the route that A* search finds from start_node to end_node
 };
 
 #endif
