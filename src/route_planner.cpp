64f21af513349dbb534419a7ded6f401b7fc0d4e
Bander Alshammari <bannadr1@hotmail.com>
2019-07-28 23:09:28 +0300

    fix buf

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -7,8 +7,8 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
     end_x *= 0.01;
     end_y *= 0.01;
 
-    start_node = &m_Model.FindClosestNode(start_x, start_y);
-    end_node = &m_Model.FindClosestNode(end_x, end_y);
+    start_node = &m_Model.FindClosestNode(start_x, end_x);
+    end_node = &m_Model.FindClosestNode(start_y, end_y);
 }
 vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node * current_node){
     vector<RouteModel::Node> path{};
